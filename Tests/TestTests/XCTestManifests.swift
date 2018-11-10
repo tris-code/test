e97a01868de9b8be43d1719aecc0366aa6cddbc8
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-11-10 01:45:32 +0000

    Delete AtomicCondition

--- Tests/TestTests/XCTestManifests.swift
+++ Tests/TestTests/XCTestManifests.swift
@@ -1,11 +1,5 @@
 import XCTest
 
-extension AtomicConditionTests {
-    static let __allTests = [
-        ("testAtomicCondition", testAtomicCondition),
-    ]
-}
-
 extension TestTests {
     static let __allTests = [
         ("testLifetime", testLifetime),
@@ -16,7 +10,6 @@ extension TestTests {
 #if !os(macOS)
 public func __allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(AtomicConditionTests.__allTests),
         testCase(TestTests.__allTests),
     ]
 }
